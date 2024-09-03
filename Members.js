8c610d2a88e85ac7fc10d751fbe56f521bb50065
Saiyyed Kaif Aalam <saiyyed.kaif.21@gmail.com>
2024-09-03 22:10:21 +0530

    Push

---
+++ Members.js
@@ -0,0 +1,8 @@
+const members = [
+    {id : 1 , name : 'Saiyyed Kaif Aalam' , email : 'saiyyedkaif@gmail.com' , status : 'active' },
+    {id : 2 , name : 'Novak Djokovic' ,email : 'novakdjokovic@gmail.com' , status : 'inactive' },
+    {id : 3 , name : 'John Doe' , email : 'johdoe@gmail.com' , status : 'active'},
+    {id : 4 , name : 'Donovon Mitchell' , email : 'donovonmitchell@gmail.com' , status : 'inactive'},
+]
+
+module.exports = members 
